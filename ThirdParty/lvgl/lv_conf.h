0b5a7421040d491ae18004fe3361f17e6123396e
Xie Yang <1023515576@qq.com>
2023-07-18 22:17:09 +0800

    将所有字体替换成 freetype

--- ThirdParty/lvgl/lv_conf.h
+++ ThirdParty/lvgl/lv_conf.h
@@ -388,10 +388,11 @@
 /*Optionally declare custom fonts here.
  *You can use these fonts as default font too and they will be available globally.
  *E.g. #define LV_FONT_CUSTOM_DECLARE   LV_FONT_DECLARE(my_font_1) LV_FONT_DECLARE(my_font_2)*/
-#define LV_FONT_CUSTOM_DECLARE LV_FONT_DECLARE(LXWKMonoEn_16)
+#define LV_FONT_CUSTOM_DECLARE extern lv_font_t *LvglTtf_GetFont();
 
 /*Always set a default font*/
-#define LV_FONT_DEFAULT &LXWKMonoEn_16
+
+#define LV_FONT_DEFAULT LvglTtf_GetFont()
 
 /*Enable handling large font and/or fonts with a lot of characters.
  *The limit depends on the font size, font face and bpp.
