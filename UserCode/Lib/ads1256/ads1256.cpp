217561e88fbaf9ca07e618b2938f005ced00f2da
Xie Yang <1023515576@qq.com>
2023-07-23 17:26:41 +0800

    芯片有时候会自动重启，怪耶

--- UserCode/Lib/ads1256/ads1256.cpp
+++ UserCode/Lib/ads1256/ads1256.cpp
@@ -21,14 +21,14 @@ static constexpr float kClkin = 7.68; // MHz, 晶振频率
 
 // 延时数据 (page 6)
 // > 50 tclkin, Delay from last SCLK edge for DIN to first SCLK rising edge for DOUT: RDATA, RDATAC, RREG Commands
-static constexpr uint32_t kT6 = 50 / kClkin + 2;
+static constexpr uint32_t kT6 = 100 / kClkin + 2;
 
 // > 8 tclkin, nCS low after final SCLK falling edge
-static constexpr uint32_t kT10 = 8 / kClkin + 2;
+static constexpr uint32_t kT10 = 16 / kClkin + 2;
 
 // Final SCLK falling edge of command to first SCLK rising edge of next command.
-static constexpr uint32_t kT11_1 = 4 / kClkin + 2;  // > 4 tclkin, for RREG, WREG, RDATA
-static constexpr uint32_t kT11_2 = 24 / kClkin + 2; // > 24 tclkin, for RDATAC, SYNC
+static constexpr uint32_t kT11_1 = 8 / kClkin + 2;  // > 4 tclkin, for RREG, WREG, RDATA
+static constexpr uint32_t kT11_2 = 48 / kClkin + 2; // > 24 tclkin, for RDATAC, SYNC
 
 void Ads1256::Init()
 {
@@ -36,6 +36,22 @@ void Ads1256::Init()
     HPT_DelayMs(100);
 
     Reset();
+    HPT_DelayMs(100);
+
+    // /**
+    //  * @brief  A/D Control Register
+    //  * Clock Out OFF
+    //  * Sensor Detect OFF (default)（可用来检测外部传感器是否完好）
+    //  * Programmable Gain Amplifier Setting = 1
+    //  */
+    // WriteReg(ADS1256_ADCON, 0x00);
+
+    // /**
+    //  * @brief GPIO Control Register
+    //  * 全部设为 Output
+    //  * 引脚电平全部设为 low
+    //  */
+    // WriteReg(ADS1256_IO, 0x00);
 
     /**
      * @brief STATUS REGISTER
@@ -43,37 +59,27 @@ void Ads1256::Init()
      * 自动校准: 开启
      * BUFEN: Buffer Enabled
      */
-    WriteReg(ADS1256_STATUS, 0x06);
-
-    /**
-     * @brief Input Multiplexer Control Register
-     * Positive Input Channel: AIN0 (default)
-     * Negative Input Channel: AINCOM
-     */
-    WriteReg(ADS1256_MUX, 0x01);
-
-    /**
-     * @brief  A/D Control Register
-     * Clock Out OFF
-     * Sensor Detect OFF (default)（可用来检测外部传感器是否完好）
-     * Programmable Gain Amplifier Setting = 1
-     */
-    WriteReg(ADS1256_ADCON, 0x00);
+    WriteReg(ADS1256_STATUS, 0x04);
 
     /**
      * @brief A/D Data Rate
      * 默认值：30,000SPS
      */
-    SetDataRate(DataRate::SPS_1000);
+    HPT_DelayMs(10);
+    WaitForDataReady();
+    SetDataRate(DataRate::SPS_100);
 
-    /**
-     * @brief GPIO Control Register
-     * 全部设为 Output
-     * 引脚电平全部设为 low
-     */
-    WriteReg(ADS1256_IO, 0x00);
+    // /**
+    //  * @brief Input Multiplexer Control Register
+    //  * Positive Input Channel: AIN0 (default)
+    //  * Negative Input Channel: AINCOM
+    //  */
+    // WriteReg(ADS1256_MUX, 0x01);
 
-    WriteCmd(ADS1256_CMD_SELFCAL); // 自校准
+    // WaitForDataReady();
+    // WriteCmd(ADS1256_CMD_SELFCAL); // 自校准
+
+    // WaitForDataReady();
 
     HPT_DelayMs(10);
 }
@@ -86,7 +92,6 @@ void Ads1256::Reset()
         HAL_GPIO_WritePin(n_reset_port_, n_reset_pin_, GPIO_PIN_SET);
         HPT_DelayMs(10);
     } else {
-        WaitForDataReady();
         WriteCmd(ADS1256_CMD_RESET);
         HPT_DelayMs(10);
     }
@@ -137,7 +142,7 @@ void Ads1256::DRDY_Callback()
                 break;
 
             default:
-                conv_queue_.at(conv_queue_index_).value = ReadDataNoWait(); // 上一个通道转换完成，读取它的值
+                auto now_index = conv_queue_index_;
 
                 // 下一个通道
                 conv_queue_index_++;
@@ -147,6 +152,9 @@ void Ads1256::DRDY_Callback()
 
                 // 开始转换下一个通道
                 SetMux(conv_queue_.at(conv_queue_index_).mux);
+
+                // 上一个通道转换完成，读取它的值
+                conv_queue_.at(now_index).value = ReadDataNoWait();
                 break;
         }
     }
@@ -222,7 +230,7 @@ void Ads1256::WriteReg(uint8_t regaddr, const uint8_t *databyte, uint8_t size)
     temp[0] = ADS1256_CMD_WREG | (regaddr & 0x0F);
     temp[1] = size - 1;
 
-    WaitForDataReady();
+    // WaitForDataReady();
     SpiWrite(temp, sizeof(temp));
 
     // Write reg
@@ -251,7 +259,6 @@ void Ads1256::ReadReg(uint8_t regaddr, uint8_t *databyte, uint8_t size)
     uint8_t temp[2];
     temp[0] = ADS1256_CMD_RREG | (regaddr & 0x0F);
     temp[1] = size - 1;
-    WaitForDataReady();
     SpiWrite(temp, sizeof(temp));
 
     HPT_DelayUs(kT6);
