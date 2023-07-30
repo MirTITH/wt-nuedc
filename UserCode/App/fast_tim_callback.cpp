578910cd9acea7b4b401c233102e1d4125330274
Xie Yang <1023515576@qq.com>
2023-07-30 19:58:08 +0800

    关闭 TestPwmRec

--- UserCode/App/fast_tim_callback.cpp
+++ UserCode/App/fast_tim_callback.cpp
@@ -6,23 +6,23 @@
 #include "control_system/pll.hpp"
 #include "Adc/adc_class_device.hpp"
 
-static control_system::Pll<float> pll(1.0 / 5000.0);
+// static control_system::Pll<float> pll(1.0 / 5000.0);
 
-static void PwmRecCallback()
-{
-    extern std::atomic<bool> kStartPwmRec;
-    extern std::atomic<float> kMod;
-    extern std::atomic<float> kDuty;
-    extern UnipolarSpwm kSpwm;
+// static void PwmRecCallback()
+// {
+//     extern std::atomic<bool> kStartPwmRec;
+//     extern std::atomic<float> kMod;
+//     extern std::atomic<float> kDuty;
+//     extern UnipolarSpwm kSpwm;
 
-    if (kStartPwmRec) {
-        auto ac_volt = Adc2.GetVoltage(0);
-        pll.Step(ac_volt);
-        kDuty = (kMod * std::cos(pll.phase_) + 1) / 2;
-        kSpwm.SetDuty(kDuty);
-        JFStream << ac_volt << pll.phase_ << kDuty << EndJFStream;
-    }
-}
+//     if (kStartPwmRec) {
+//         auto ac_volt = Adc2.GetVoltage(0);
+//         pll.Step(ac_volt);
+//         kDuty = (kMod * std::cos(pll.phase_) + 1) / 2;
+//         kSpwm.SetDuty(kDuty);
+//         JFStream << ac_volt << pll.phase_ << kDuty << EndJFStream;
+//     }
+// }
 
 uint32_t kFastTimCallbackDuration;
 uint32_t kFastTimCallbackCount = 0;
@@ -33,5 +33,5 @@ void FastTimCallback()
     TimeMeter time_meter(&kFastTimCallbackDuration);
     kFastTimCallbackCount++;
 
-    PwmRecCallback();
+    // PwmRecCallback();
 }
